8b34ad6f6bb923dfd3ad8924666deeabb669e015
ihsan <ihsaneddin@gmail.com>
2019-07-04 12:24:43 +0700

    add null presenter

--- src/Presenters/Fractal.php
+++ src/Presenters/Fractal.php
@@ -2,11 +2,38 @@
 namespace Support\Presenters;
 
 use Prettus\Repository\Presenter\FractalPresenter;
-use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Pagination\AbstractPaginator;
+use League\Fractal\Manager;
+use League\Fractal\Resource\NullResource;
 
 abstract class Fractal extends FractalPresenter {
 
   abstract public function getTransformer();
+
+  public function present($data)
+  {
+    if (!class_exists('League\Fractal\Manager')) {
+      throw new Exception(trans('repository::packages.league_fractal_required'));
+    }
+
+    if ($data instanceof EloquentCollection) {
+      $this->resource = $this->transformCollection($data);
+    } elseif ($data instanceof AbstractPaginator) {
+      $this->resource = $this->transformPaginator($data);
+    } else {
+      if(is_null($data)){
+        $this->resource = $this->transformNull($data);
+      }else{
+        $this->resource = $this->transformItem($data);
+      }
+    }
+    return $this->fractal->createData($this->resource)->toArray();
+  }
+
+
+  protected function transformNull($data){
+    return new NullResource($data, $this->getTransformer());
+  }
 
 }
