5f4194ae8236487aeac86d676c7ba69b27bc99ed
ihsan <ihsaneddin@gmail.com>
2019-07-25 22:05:22 +0700

    add getRedisConnectionName

--- src/Entities/Traits/CacheableFieldByRedisTrait.php
+++ src/Entities/Traits/CacheableFieldByRedisTrait.php
@@ -6,32 +6,32 @@ use Illuminate\Support\Facades\Redis;
 trait CacheableFieldByRedisTrait {
 
   public function storeOnCache(string $key, $value){
-    if ($this->getRedisConnectionName()){
-      return Redis::connection($this->getRedisConnectionName())->set($key, $value);
+    if ($this->redis_connection_name){
+      return Redis::connection($this->redis_connection_name)->set($key, $value);
     }else{
       return Redis::set($key, $value);
     }
   }
 
   public function fetchFromCache(string $key){
-    if ($this->getRedisConnectionName()){
-      return Redis::connection($this->getRedisConnectionName())->get($key);
+    if ($this->redis_connection_name){
+      return Redis::connection($this->redis_connection_name)->get($key);
     }else{
       return Redis::get($key);
     }
   }
 
   public function removeFromCache(string $key){
-    if ($this->getRedisConnectionName()){
-      return Redis::connection($this->getRedisConnectionName())->del($key);
+    if ($this->redis_connection_name){
+      return Redis::connection($this->redis_connection_name)->del($key);
     }else{
       return Redis::del($key);
     }
   }
 
   public function getRedisConnectionNameAttribute(){
-    if(property_exists($this, "getRedisConnectionName()")){
-      return $this->getRedisConnectionName();
+    if(property_exists($this, "redis_connection_name")){
+      return $this->redis_connection_name;
     }
     if(method_exists($this, "getRedisConnectionName")){
       return $this->getRedisConnectionName();
@@ -40,10 +40,6 @@ trait CacheableFieldByRedisTrait {
 
   public static function isKeyExistsonRedis(string $key){
     return (new static)->fetchFromCache($key);
-  }
-
-  public function getRedisConnectionName(){
-    return $this->getRedisConnectionName();
   }
 
 }
