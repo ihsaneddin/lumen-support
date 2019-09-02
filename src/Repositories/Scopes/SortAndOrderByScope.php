c90b27fae9365a7a08938e1275261147b9b5e6ce
ihsan <ihsaneddin@gmail.com>
2019-09-02 16:22:02 +0700

    fix typo

--- src/Repositories/Scopes/SortAndOrderByScope.php
+++ src/Repositories/Scopes/SortAndOrderByScope.php
@@ -9,8 +9,8 @@ class SortAndOrderByScope implements CriteriaInterface
 
   public function apply($model, RepositoryInterface $repository)
   {
-    $sort = request()->get("sort", "asc");
-    $order = request()->get("order", "created_at");
+    $sort = request()->get("sort", "created_at");
+    $order = request()->get("order", "asc");
     return $model->orderBy($sort, $order);
   }
 
